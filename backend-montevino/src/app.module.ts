f0f59660a26db7e3a2f10f326a393e8b618c722a
VeraMiguelAngel <miguel_vera_a@yahoo.com.ar>
2026-03-12 01:59:47 -0300

    Corrección: compilación de configuración y URL de base de datos para Render

--- backend-montevino/src/app.module.ts
+++ backend-montevino/src/app.module.ts
@@ -2,7 +2,7 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/users/users.module';
-import typeorm from './config/typeorm';
+import typeormConfig from './config/typeorm';
 import { ReservationsModule } from './modules/reservations/reservations.module';
 import { PlatosModule } from './modules/platos/platos.module';
 import { CategoriesModule } from './modules/categories/categories.module';
@@ -13,13 +13,14 @@ import { AuthModule } from './modules/auth/auth.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [typeorm],
     }),
     TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => config.get('typeorm')!,
+      useFactory: () => ({
+        ...typeormConfig(), // usa la config que definimos en typeorm.ts
+      }),
     }),
-
     UsersModule,
     ReservationsModule,
     PlatosModule,
@@ -27,7 +28,10 @@ import { AuthModule } from './modules/auth/auth.module';
     PedidosModule,
     AuthModule,
   ],
-  controllers: [],
-  providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  configure(consumer: MiddlewareConsumer) {
+    // si más adelante necesitás middlewares, los agregás acá
+  }
+}
