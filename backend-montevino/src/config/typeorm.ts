078c6547b0b5a99413ec9c91869aaffda239f361
Valentin <valenvfx04@gmail.com>
2026-03-18 18:05:26 -0300

    Correccion error deploy y borrado deploy

--- backend-montevino/src/config/typeorm.ts
+++ backend-montevino/src/config/typeorm.ts
@@ -2,32 +2,21 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import { config as dotenvConfig } from 'dotenv';
 import { registerAs } from '@nestjs/config';
 
-dotenvConfig({ path: '.development.env' });
+dotenvConfig({ path: '.env' });
 
-const isProd = process.env.NODE_ENV === 'production';
-
-export const config: DataSourceOptions = isProd
-  ? {
-      type: 'postgres',
-      url: process.env.DATABASE_URL, // Render la provee
-      synchronize: true,
-      logging: false,
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      migrations: ['dist/migrations/*{.ts,.js}'],
-    }
-  : {
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5433,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      dropSchema: true,
-      synchronize: true,
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      migrations: ['dist/migrations/*{.ts,.js}'],
-    };
+export const config = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  dropSchema: true,
+  synchronize: true,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
+};
 
 export default registerAs('typeorm', () => config);
 
-export const connectionSource = new DataSource(config);
+export const connectionSource = new DataSource(config as DataSourceOptions);
