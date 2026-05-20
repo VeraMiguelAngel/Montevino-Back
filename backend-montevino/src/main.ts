51738125fcd64c7969d4cf5bba1ade5ceafe5179
VeraMiguelAngel <miguel_vera_a@yahoo.com.ar>
2026-05-20 12:33:39 -0300

    new modification to main.ts to test the deployment

--- backend-montevino/src/main.ts
+++ backend-montevino/src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const FRONTURL = process.env.FRONTEND_URL || 'http://localhost:3001';gi
+  const FRONTURL = process.env.FRONTEND_URL || 'http://localhost:3001';
 
   app.useGlobalPipes(
     new ValidationPipe({
